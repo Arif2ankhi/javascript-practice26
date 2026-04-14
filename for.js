a9d60cf202e757b3b0cf0b9be07a23d94031eeea
Arif2ankhi <arif2ankhi@gmail.com>
2026-04-14 08:12:47 +0100

    seconds commit

---
+++ for.js
@@ -0,0 +1,14 @@
+// let num = 1;
+// while (num <5){
+//     console.log(num)
+//     num++;
+// }
+
+for (let num = 0; num < 20; num++){
+    if(num % 2 ===1)
+        console.log('odd number');
+    console.log(num);
+}
+ for ( let i = 1; i < 15; i=i+2){
+    console.log(i);
+ }
